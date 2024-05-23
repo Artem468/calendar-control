809fe2f614684c06baaa34e717ded19b61634a6b
Artem468 <56316884+Artem468@users.noreply.github.com>
2024-05-23 15:51:32 +0200

    Add files via upload

--- calendar_control.py
+++ calendar_control.py
@@ -7,18 +7,18 @@ from dateutil import relativedelta
 
 class FletCalendar(ft.UserControl):
 
-    def __init__(self, page, width=355, height=300, on_date_click=None, mark_days: list[tuple[int, int, int]] = None):
+    def __init__(self, page, width=355, height=300, on_date_click=None, mark_days: list[tuple[int, int, int]] = None,
+                 border_color=ft.colors.CYAN_600, text_color=ft.colors.CYAN_600, current_day_color=ft.colors.GREY_300):
         super().__init__()
         today = datetime.datetime.today()
         self.now_month = today.month
         self.current_month = today.month
         self.current_day = today.day
         self.current_year = today.year
-        self.text_color = None
-        self.mark_day_color = None
-        self.border_color = None
+        self.border_color = border_color
+        self.text_color = text_color
+        self.mark_day_color = current_day_color
         self.page = page
-        self.set_theme()
         self.width = width
         self.height = height
         self.on_date_click = on_date_click
@@ -29,7 +29,6 @@ class FletCalendar(ft.UserControl):
                                                alignment=ft.alignment.bottom_center)
         self.mark_days = [] if mark_days is None else mark_days
         self.build()
-
 
     def set_current_date(self):
         """Set the calendar to the current date."""
@@ -67,13 +66,6 @@ class FletCalendar(ft.UserControl):
         """Get the calendar from the calendar module."""
         cal = calendar.HTMLCalendar()
         return cal.monthdayscalendar(self.current_year, self.current_month)
-
-    def set_theme(self, border_color=ft.colors.CYAN_600,
-                  text_color=ft.colors.CYAN_600,
-                  current_day_color=ft.colors.GREY_300):
-        self.border_color = border_color
-        self.text_color = text_color
-        self.mark_day_color = current_day_color
 
     def build(self):
         """Build the calendar for flet."""
